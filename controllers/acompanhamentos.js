ed94fa3d75e8da75cdc5f365c2c2c22ccd026c3e
Juliano <juliano-gema@hotmail.com>
2019-02-17 16:55:33 -0300

    iniciando api salv

--- controllers/acompanhamentos.js
+++ controllers/acompanhamentos.js
@@ -1,20 +1,22 @@
-const { AcompanhametosModel } = require('./../models')
+const { AcompanhamentosModel } = require('./../models/acompanhamentos')
+const { AcompanhamentosFuncionarioModel } = require('./../models/acompanhamento_funcionarios')
+const { AcompanhamentosResidenteModel } = require('./../models/acompanhamento_residente')
 
 class Acompanhamento {
 
-    getAll(req, res) {
+    getAll (req, res) {
         AcompanhamentosModel.findAll({ raw: true })
             .then(acompanhamento => res.json(acompanhamento))
             .catch(error => res.json(error))
     }
 
-    getById(req, res) {
+    getById (req, res) {
         AcompanhamentosModel.findById(req.params.id)
             .then(acompanhamento => res.json(acompanhamento))
             .catch(error => res.json(error))
     }
 
-    create(req, res) {
+    create (req, res) {
         AcompanhamentosModel.create(req.body, (error, acompanhamento) => {
             if (error)
                 res.send(error)
@@ -23,8 +25,8 @@ class Acompanhamento {
         })
     }
     
-    update(req, res) {
-        AcompanhametosModel.update({_id: req.params.id}, {$set: req.body}, (error, acompanhamento) => {
+    update (req, res) {
+        AcompanhamentosModel.update({_id: req.params.id}, {$set: req.body}, (error, acompanhamento) => {
             if(error)
                 res.send(error)
             else 
@@ -33,8 +35,8 @@ class Acompanhamento {
       }
         
         
-        delete(req, res) {
-            AcompanhametosModel.del(req.params.id, (error, acompanhamento) => {
+        delete (req, res) {
+            AcompanhamentosModel.del(req.params.id, (error, acompanhamento) => {
                 if(error)
                     res.send(error)
                 else
@@ -44,4 +46,43 @@ class Acompanhamento {
     }
 
 
+
+
+class AcompanhamentoFuncionario{
+
+    getAll (req, res) {
+        AcompanhamentosFuncionarioModel.findAll({ raw: true })
+            .then(acompanhamento_funcionario => res.json(acompanhamento_funcionario))
+            .catch(error => res.json(error))
+    }
+
+    getById (req, res) {
+        AcompanhamentosFuncionarioModel.findById(req.params.id)
+            .then(acompanhamento_funcionario => res.json(acompanhamento_funcionario))
+            .catch(error => res.json(error))
+    }
+    
+    
+}
+
+
+class AcompanhamentoResidente{
+
+    getAll (req, res) {
+        AcompanhamentosResidenteModel.findAll({ raw: true })
+            .then(acompanhamento_residente => res.json(acompanhamento_residente))
+            .catch(error => res.json(error))
+    }
+
+    getById (req, res) {
+        AcompanhamentosResidenteModel.findById(req.params.id)
+            .then(acompanhamento_residente => res.json(acompanhamento_residente))
+            .catch(error => res.json(error))
+    }
+    
+    
+}
+
 module.exports = new Acompanhamento()
+module.exports = new  AcompanhamentoFuncionario()
+module.exports = new  AcompanhamentoResidente()
---
+++ routes/acompanhamentos.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const route = express.Router()
+
+const AcompanhamentoController = require('./../controllers/acompanhamentos')
+
+route.get('/acompanhamento', AcompanhamentoController.getAll)
+route.get('/acompanhamento/:id', AcompanhamentoController.getById)
+route.post('/acompanhamento', AcompanhamentoController.create)
+route.put('/acompanhamento/:id', AcompanhamentoController.update)
+route.delete('/acompanhamento/:id', AcompanhamentoController.delete)
+
+
+
+const AcompanhamentoFuncionarioController = require('./../controllers/acompanhamentos')
+
+route.get('/acompanhamento_funcionario', AcompanhamentoFuncionarioController.getAll)
+route.get('/acompanhamento_funcionario/:id', AcompanhamentoFuncionarioController.getById)
+
+
+
+const AcompanhamentoResidenteController = require('./../controllers/acompanhamentos')
+
+route.get('/acompanhamento_residente', AcompanhamentoResidenteController.getAll)
+route.get('/acompanhamento_residente/:id', AcompanhamentoResidenteController.getById)
+
+
+module.exports = route
