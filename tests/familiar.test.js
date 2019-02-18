0d8997bec3b18a3e2c7d458ed35f6677398b8684
hppod <hppodgornik@gmail.com>
2019-02-18 15:38:44 -0300

    Mocando valores para teste

--- tests/familiar.test.js
+++ tests/familiar.test.js
@@ -4,3 +4,31 @@
  * file: familiar.test.js
  * descrition: Arquivo de teste automatizado da API SALV. Métodos "familiar" testados
  */
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const expect = chai.expect
+chai.use(chaiHttp)
+
+const app = require('./../app')
+//MODEL FAMILIAR (ADICIONAR AO SER FEITO)
+
+const MOCK_FAMILIAR_DEFAULT = {
+    NOME: 'VALDECIR',
+    SOBRENOME: 'MATOS',
+    PARENTESCO: 'FILHO',
+}
+
+let MOCK_FAMILIAR_ERROR = {
+    NOME: 'YARA',
+    SOBRENOME: 'MARTINS'
+}
+
+const MOCK_FAMILIAR_ATUALIZAR = {
+    NOME: 'RAFAEL',
+    SOBRENOME: 'JORGE',
+    PARENTESCO: 'SOBRINHO'
+}
+
+let MOCK_FAMILIAR_CODIGO
+
