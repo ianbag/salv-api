3f526f8a1812e73115f881a2ef9d64941ab9973b
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-02-20 11:36:42 -0300

    Estruturado testes da API de Beneficio

--- tests/beneficio.test.js
+++ tests/beneficio.test.js
@@ -2,7 +2,7 @@
  * @author Ian Rotondo Bagliotti
  * @email ian.bagliotti@gmail.com
  * @create date 2019-02-19 11:46:29
- * @modify date 2019-02-19 12:14:08
+ * @modify date 2019-02-19 11:46:29
  * @desc Arquivo de testes da API de Beneficio
  */
 
@@ -11,7 +11,7 @@ const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
 const expect = chai.expect
 
-const { BeneficioModel } = require('./../app/models')
+const { BeneficioModel, ResidenteModel, PessoaModel } = require('./../app/models')
 const app = require('./../app')
 
 let MOCK_BENEFICIO_NOME
@@ -36,25 +36,69 @@ const MOCK_BENEFICIO_CADASTRAR = {
     PROVA_VIDA_BENEFICIO: '2020-08-22'
 }
 
+// MOCK DOS RELACIONAMENTOS
+const MOCK_PESSOA_DEFAULT = {
+    CODIGO: 1,
+    NOME: 'Ian',
+    SOBRENOME: 'Rotondo Bagliotti',
+    RG: '468915217',
+    CPF: '84404069405',
+    SEXO: 'm',
+    ESTADO_CIVIL: 's',
+    DATA_NASCIMENTO: '2000-01-30',
+    RELIGIAO: 'cat',
+    ESCOLARIDADE: 'sc'
+}
 
-describe('TDD Beneficio', function(){
-    this.beforeAll(async() => {
+const MOCK_RESIDENTE_DEFAULT = {
+    APELIDO: 'Lobo',
+    PROFISSAO: 'Analista e Desenvolvedor de Sistemas',
+    TITULO_ELEITOR: '1232131',
+    ZONA_ELEITORAL: '3AB',
+    SECAO_ELEITORAL: '11',
+    NUMERO_CERTIDAO_NASCIMENTO: '1234',
+    FOLHA_CERTIDAO_NASCIMENTO: '22',
+    LIVRO_CERTIDAO_NASCIMENTO: '5',
+    CIDADE_CERTIDAO_NASCIMENTO: 'Taquaritinga',
+    ESTADO_CERTIDAO_NASCIMENTO: 'SP',
+    CARTAO_SAMS: '153445131',
+    CARTAO_SUS: '5189756891713',
+    NUMERO_INSS: '123',
+    BANCO_INSS: 'Banco do Brasil',
+    AGENCIA_INSS: '0001',
+    CONTA_INSS: '145165125 5',
+    VALOR_INSS: 1000.50,
+    SITUACAO_INSS: 'BCP',
+    PROVA_VIDA_INSS: '2019-02-27',
+    DATA_ACOLHIMENTO: '2018-05-07',
+    DATA_DESACOLHIMENTO: null,
+    MOTIVO_DESACOLHIMENTO: null,
+    PESSOA_CODIGO: 1
+}
+
+
+describe.only('TDD Beneficio', function () {
+    this.beforeAll(async () => {
+        await BeneficioModel.destroy({ where: {} })
+        await ResidenteModel.destroy({ where: {} })
+        await PessoaModel.destory({ where: {} })
+        await PessoaModel.create(MOCK_PESSOA_DEFAULT)
+        await ResidenteModel.create(MOCK_RESIDENTE_DEFAULT)
         const result = await BeneficioModel.create(MOCK_BENEFICIO_DEFAULT)
         MOCK_BENEFICIO_NOME = result.NOME_BENEFICIO
     })
-    
+
     describe('/GET: ', () => {
         it('Deve retornar os Beneficios existente no banco de dados', (done) => {
             chai.request(app)
                 .get('/beneficio')
                 .end((error, res) => {
-
                     expect(res.statusCode).to.eql(200)
                     done()
-            })
+                })
         })
     })
-    
+
     describe('/GET/NOME: ', () => {
         it('Deve retornar um Beneficios pelo NOME', (done) => {
             chai.request(app)
@@ -63,7 +107,7 @@ describe('TDD Beneficio', function(){
 
                     expect(res.statusCode).to.eql(200)
                     done()
-            })
+                })
         })
     })
 
@@ -76,7 +120,7 @@ describe('TDD Beneficio', function(){
 
                     expect(res.statusCode).to.eql(200)
                     done()
-            })
+                })
         })
 
         it('Deve retornar erro ao adicionar Beneficio, por faltar o campo NOME_BENEFICIO', (done) => {
@@ -88,8 +132,33 @@ describe('TDD Beneficio', function(){
 
                     expect(res.statusCode).to.eql(200)
                     done()
-            })
+                })
         })
     })
 
+    describe('/PUT: ', () => {
+        it('Deve atualizar uma pessoa pelo NOME: ', (done) => {
+            chai.request(app)
+                .put(`/beneficio/${MOCK_BENEFICIO_NOME}`)
+                .send(MOCK_BENEFICIO_CADASTRAR)
+                .end((error, res) => {
+
+                    expect(res.statusCode).to.eql(200)
+                    done()
+                })
+        })
+    })
+
+    describe('/DELETE: ', () => {
+        it('Deve atualizar uma pessoa pelo NOME: ', (done) => {
+            chai.request(app)
+                .delete(`/beneficio/${MOCK_BENEFICIO_NOME}`)
+                .send(MOCK_BENEFICIO_CADASTRAR)
+                .end((error, res) => {
+
+                    expect(res.statusCode).to.eql(200)
+                    done()
+                })
+        })
+    })
 })
