f802f86120a20cfae3b9a98565dc27000237033e
hppod <hppodgornik@gmail.com>
2019-02-20 01:09:08 -0300

    Controller telefone sem métodos desenvolvidos

---
+++ app/controllers/telefone.js
@@ -0,0 +1,32 @@
+/**
+ * author: hppod
+ * create: 20/02/2019 01h07
+ * file: controllers/telefone.js
+ */
+
+const { TelefoneModel } = require('./../models')
+
+class Telefone {
+
+    get(req, res) {
+
+    }
+
+    getById(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
+}
+
+module.exports = new Telefone()
