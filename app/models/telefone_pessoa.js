4894b116ce2544d2a63f913eb939613ab967f1c0
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-02-20 19:45:48 -0300

    Criado arquivo Model de TELEFONE_PESSOA

---
+++ app/models/telefone_pessoa.js
@@ -0,0 +1,26 @@
+/**
+ * @author Ian Rotondo Bagliotti
+ * @email ian.bagliotti@gmail.com
+ * @create date 2019-02-20 19:42:40
+ * @modify date 2019-02-20 19:42:40
+ * @desc Arquivo model da API de TELEFONE_PESSOA
+ */
+
+module.exports = (sequelize, DataTypes) => {
+    const TelefonePessoaModel = sequelize.define('TelefonePessoaModel', {
+        CODIGO_PESSOA: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        TELEFONE_CODIGO: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+        {
+            tableName: 'TELEFONE_PESSOA',
+            timestamps: false
+        }
+    )
+    return TelefonePessoaModel
+}
