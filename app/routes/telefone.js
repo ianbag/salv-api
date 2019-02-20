8b47a055d55d81309c8924cff8418c5ee0e961b8
hppod <hppodgornik@gmail.com>
2019-02-20 01:11:22 -0300

    Routes de telefone

---
+++ app/routes/telefone.js
@@ -0,0 +1,16 @@
+/**
+ * author: hppod
+ * create: 20/02/2019 01h09
+ * file: routes/telefone.js
+ */
+
+const express = require('express')
+const route = express.Router()
+
+const TelefoneController = require('./../controllers/telefone')
+
+route.get('/telefone', TelefoneController.get)
+route.get('/telefone/:id', TelefoneController.getById)
+route.post('/telefone', TelefoneController.create)
+route.put('/telefone/:id', TelefoneController.update)
+route.delete('/telefone/:id', TelefoneController.delete)
