aa421e7ea23906ffb7481d07df8de876299ebec0
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-02-20 19:47:40 -0300

    Criado arquivo controller telefone_pessoa sem os métodos

---
+++ app/controllers/telefone_pessoa.js
@@ -0,0 +1,32 @@
+/**
+ * @author Ian Rotondo Bagliotti
+ * @email ian.bagliotti@gmail.com
+ * @create date 2019-02-20 19:46:04
+ * @modify date 2019-02-20 19:46:04
+ * @desc Arquivo Controller da API de Telefone Pessoa
+ */
+const { TelefonePessoaModel } = require('./../models')
+
+class TelefonePessoa {
+    get(req, res) {
+
+    }
+
+    getById(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
+}
+
+module.exports = new TelefonePessoa() 
