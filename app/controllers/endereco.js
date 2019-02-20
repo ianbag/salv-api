ddc7455ed02a800cd0adfcbbc02bb0c44b73c4ba
hppod <hppodgornik@gmail.com>
2019-02-19 23:37:23 -0300

    Criação do arquivo controllers de endereço

---
+++ app/controllers/endereco.js
@@ -0,0 +1,32 @@
+/**
+ * author: hppod
+ * create: 19/02/2019 23h35
+ * file: controllers/endereco.js
+ */
+
+const { EnderecoModel } = require('./../models')
+
+class Endereco {
+
+    get(req, res){
+
+    }
+
+    getById(req, res){
+
+    }
+
+    create(req, res){
+
+    }
+
+    update(req, res){
+
+    }
+
+    delete(req, res){
+        
+    }
+}
+
+module.exports = new Endereco()
