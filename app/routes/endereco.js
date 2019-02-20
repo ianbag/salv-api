c4d6a619fa407621a4e322a55a77028a7e42c079
hppod <hppodgornik@gmail.com>
2019-02-19 23:40:54 -0300

    Criação do arquivo routes para endereço

---
+++ app/routes/endereco.js
@@ -0,0 +1,16 @@
+/**
+ * author: hppod
+ * create: 19/02/2019 20h39
+ * file: routes/endereco.js
+ */
+
+const express = require('express')
+const route = express.Router()
+
+const EnderecoController = require('./../controllers/endereco')
+
+route.get('/endereco', EnderecoController.get)
+route.get('/endereco/:id', EnderecoController.getById)
+route.post('/endereco', EnderecoController.create)
+route.put('/endereco/:id', EnderecoController.update)
+route.delete('/endereco/:id', EnderecoController.delete)
