106508d635f63ca230e495207507b10adf614de3
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-02-21 14:15:25 -0300

    criação do controller de funcionario

---
+++ app/controllers/funcionario.js
@@ -0,0 +1,31 @@
+/**
+ * author: NathanBarsoti8
+ */
+
+ const { FuncionarioModel } = require('./../models')
+
+ class Funcionario {
+     
+    get(req, res){
+
+    }
+
+    getById(req, res){
+
+    }
+
+    create(req, res){
+
+    }
+
+    update(req, res){
+
+    }
+
+    delete(req, res){
+
+    }
+
+ }
+
+ module.exports = new Funcionario()
