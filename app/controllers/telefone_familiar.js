6c7f3ccc082153415dff6a2f01ffdfc4194a4be4
hppod <hppodgornik@gmail.com>
2019-02-21 14:27:08 -0300

    Controller telefone_familiar sem métodos desenvolvidos

---
+++ app/controllers/telefone_familiar.js
@@ -0,0 +1,32 @@
+/**
+ * author: hppod
+ * create: 21/02/2019 14h25
+ * file: controllers/telefone_familiar.js
+ */
+
+const { TelefoneFamiliarModel } = require('./../models')
+
+class TelefoneFamiliar {
+
+    get(req, res) {
+
+    }
+
+    getById(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
+}
+
+module.exports = new TelefoneFamiliar()
