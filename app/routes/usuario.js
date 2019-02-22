5e731467f122ad5be787fb51b2472983825784d6
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-02-22 11:03:33 -0300

    criação das rotas de usuario

---
+++ app/routes/usuario.js
@@ -0,0 +1,16 @@
+/**
+ * author: NathanBarsoti8
+*/
+
+const express = require('express')
+const route = express.Router()
+
+const UsuarioController = require('./../controllers/usuario')
+
+route.get('/usuario', UsuarioController.get)
+route.get('/usuario/:id', UsuarioController.getById)
+route.get('/usuario', UsuarioController.create)
+route.get('/usuario', UsuarioController.update)
+route.get('/usuario', UsuarioController.delete)
+
+module.exports = route
