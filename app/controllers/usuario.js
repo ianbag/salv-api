0d315da4923bcf75e663e414941ea731e03a1340
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-02-23 11:06:25 -0300

    att controllers of usuario

--- app/controllers/usuario.js
+++ app/controllers/usuario.js
@@ -7,20 +7,36 @@
  class Usuario {
      
     getById(req, res){
-
+        UsuarioModel.findByPk(req.params.id)
+            .then(usuario => res.json(usuario))
+            .catch(error => res.json(error))
     }
 
     create(req, res){
-
+        UsuarioModel.create(req.body)
+            .then(usuario => res.json(usuario))
+            .catch(error => res.json(error))
     }
 
     update(req, res){
-
+        UsuarioModel.destroy({
+            where: {
+                USUARIO: req.params.id
+            }
+        })
+            .then(usuario => res.json(usuario))
+            .catch(error => res.json(error))
     }
 
     delete(req, res){
-
-    }
+        UsuarioModel.destroy({
+            where: {
+                USUARIO: req.params.id
+            }
+        })
+            .then(usuario => res.json(usuario))
+            .catch(error => res.json(error))
+    }   
 
  }
 
