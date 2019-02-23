d368dc4e645706f3f1418946b57329299b049d36
hppod <hppodgornik@gmail.com>
2019-02-22 22:53:19 -0300

    Controller endereco_familiar ok, no methods

---
+++ app/controllers/endereco_familiar.js
@@ -0,0 +1,24 @@
+/**
+ * author: hppod
+ * create: 22/02/2019 22h51
+ * file: controllers/endereco_familiar.js
+ */
+
+const { EnderecoFamiliarModel } = require('./../models')
+
+class EnderecoFamiliar {
+
+    getById(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
+}
+
+module.exports = new EnderecoFamiliar()
