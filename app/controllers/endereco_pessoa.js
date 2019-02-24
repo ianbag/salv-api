cd2ccd9ef5eb4543459adfac22bc987024301b04
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-02-24 14:36:11 -0300

    Desenvolvido metodos do Controller de Endereco Pessoa

---
+++ app/controllers/endereco_pessoa.js
@@ -0,0 +1,29 @@
+/**
+ * @author Ian Rotondo Bagliotti
+ * @email ian.bagliotti@gmail.com
+ * @create date 2019-02-24 14:00:28
+ * @modify date 2019-02-24 14:35:45
+ * @desc Arquivo Controller da API de Endereco Pessoa
+ */
+
+const { EnderecoPessoaModel } = require('./../models')
+
+ class EnderecoPessoa {
+     getByID(req, res) {
+        EnderecoPessoaModel.findByPk(req.params.id)
+        .then(enderecoPessoa => res.json(enderecoPessoa))
+        .catch(error => res.json(error))
+     }
+     create(req, res) {
+        EnderecoPessoaModel.create(req.body)
+        .then(enderecoPessoa => res.json(enderecoPessoa))
+        .catch(error => res.json(error))
+     }
+     delete(req, res) {
+        EnderecoPessoaModel.destroy({ where: { PESSOA_CODIGO: req.params.id } })
+            .then(enderecoPessoa => res.json(enderecoPessoa))
+            .catch(error => res.json(error))
+     }
+ }
+
+ module.exports = new EnderecoPessoa()
