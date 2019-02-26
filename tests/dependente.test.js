42e19771cf756cde505f9534222fe8c5580dd4d3
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-02-26 09:26:06 -0300

    criação dos tests de dependente

--- tests/dependente.test.js
+++ tests/dependente.test.js
@@ -63,3 +63,110 @@
  let MOCK_DEPENDENTE_CODIGO
 
  //Início dos tests
+
+ describe('TDD Dependente', function() {
+    this.beforeAll(async () => {
+
+        const funcionario = await FuncionarioModel.create(MOCK_FUNCIONARIO_DEFAULT)
+
+        console.log('Add valores ao mock')
+        MOCK_DEPENDENTE_DEFAULT.FUNCIONARIO.CODIGO = funcionario.CODIGO 
+
+        const result = await DependenteModel.create(MOCK_DEPENDENTE_DEFAULT)
+
+        MOCK_DEPENDENTE_FUNCIONARIO_CODIGO = result.FUNCIONARIO_CODIGO
+
+        console.log('DEPENDENTE ' + MOCK_DEPENDENTE_FUNCIONARIO_CODIGO)
+    })
+
+    //GET ID
+    describe('/GET/ID: ', () => {
+        it('Deve retornar um dependente com base no ID dele (funcionario)', (done) => {
+            chai.request(app)
+                .get(`/dependente/${MOCK_DEPENDENTE_FUNCIONARIO_CODIGO}`)
+                .end((error, res) => {
+                    const result = res.body
+                    expect(res.statusCode).to.eql(200)
+                    expect(result).to.eql(MOCK_DEPENDENTE_DEFAULT)
+                    done()
+                })
+        })
+    })
+
+    //POST
+    describe('/POST: ', () => {
+        this.beforeAll(async () => {
+            const funcionario = await FuncionarioModel.create(MOCK_FUNCIONARIO_DEFAULT)
+
+            console.log('Add valores ao mock')
+            MOCK_DEPENDENTE_CADASTRAR.FUNCIONARIO_CODIGO = funcionario.CODIGO
+        })
+
+        it('Deve adicionar um dependente a um funcionaio no banco de dados', (done) => {
+            chai.request(app)
+                .post('/usuario')
+                .send(MOCK_DEPENDENTE_CADASTRAR)
+                .end((error, res) => {
+                    const result = res.body
+                    expect(res.statusCode).to.eql(200)
+                    expect(result).to.eql(MOCK_DEPENDENTE_CADASTRAR)
+                    done()
+                })
+        })
+
+        this.beforeAll(async () => {
+            const funcionaio = await FuncionarioModel.create(MOCK_FUNCIONARIO_DEFAULT)
+
+            console.log('Add valores ao mock')
+            MOCK_DEPENDENTE_ERROR.FUNCIONARIO_CODIGO = funcionaio.CODIGO
+        })
+
+        it('Deve retornar erro ao tentar adicionar um dependente que esteja com campo obrigatório em branco', (done) => {
+            chai.request(app)
+                .post('/usuario')
+                .send(MOCK_DEPENDENTE_ERROR)
+                .end((error, res) => {
+                    const [result] = res.body.errors
+                    expect(res.statusCode).to.eql(200)
+                    expect(result.path).to.eql('FOLHA_CERTIDAO_NASCIMENTO')
+                    expect(result.type).to.eql('notNull Violation')
+                    done()
+                })
+        })
+    })
+
+    //PUT
+    describe('/PUT/ID: ', () => {
+        this.beforeAll(async () => {
+            const funcionario = await FuncionarioModel.create(MOCK_FUNCIONARIO_DEFAULT)
+
+            console.log('Add valores ao mock')
+            MOCK_DEPENDENTE_CADASTRAR.FUNCIONARIO_CODIGO = funcionario.CODIGO
+        })
+
+        it('Atualizar os dados do dependente de acordo com o seu id', (done) => {
+            chai.request(app)
+                .put(`/dependente/${MOCK_DEPENDENTE_FUNCIONARIO_CODIGO}`)
+                .send(MOCK_DEPENDENTE_ATUALIZAR)
+                .end((error, res) => {
+                    expect(res.statusCode).to.eql(200)
+                    expect(res.body).to.eql([1])
+                    done()
+                })
+        })
+    })
+
+    //DELETE
+    describe('/DELETE/ID: ', () => {
+        it('Deve apagar um dependente com base no seu id', (done) => {
+            chai.request(app)
+                .delete(`/dependente/${MOCK_DEPENDENTE_FUNCIONARIO_CODIGO}`)
+                .end((error, res) => {
+                    expect(res.statusCode).to.eql(200)
+                    expect(res.body).to.eql(1)
+                    done()
+                })
+        })
+    })
+
+ })
