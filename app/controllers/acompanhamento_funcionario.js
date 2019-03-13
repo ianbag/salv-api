22a265942671bb4a5a70ed0ddf252fff85711cc4
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-03-13 01:10:22 -0300

    Corrigido API de Acompanhamento e feita associaçao nos metodos get

--- app/controllers/acompanhamento_funcionario.js
+++ app/controllers/acompanhamento_funcionario.js
@@ -1,15 +1,28 @@
-const { AcompanhamentosFuncionarioModel } = require('./../models/acompanhamento_funcionario')
+const { AcompanhamentoFuncionarioModel, AcompanhamentosModel, FuncionarioModel } = require('./../models/')
+
+AcompanhamentoFuncionarioModel.belongsTo(AcompanhamentosModel, {as: 'ACOMPANHAMENTO', foreignKey: 'ACOMPANHAMENTO_CODIGO'})
+AcompanhamentoFuncionarioModel.belongsTo(FuncionarioModel, {as: 'FUNCIONARIO', foreignKey: 'FUNCIONARIO_CODIGO'})
 
 class AcompanhamentoFuncionario{
 
-    getAll (req, res) {
-        AcompanhamentosFuncionarioModel.findAll({ raw: true })
+    getById (req, res) {
+        AcompanhamentoFuncionarioModel.findById(req.params.id, {
+            include: [
+                {model: AcompanhamentosModel, as: 'ACOMPANHAMENTO'},
+                {model: FuncionarioModel, as: 'FUNCIONARIO'}
+            ]
+        })
             .then(acompanhamento_funcionario => res.json(acompanhamento_funcionario))
             .catch(error => res.json(error))
     }
 
-    getById (req, res) {
-        AcompanhamentosFuncionarioModel.findById(req.params.id)
+    create(req, res) {
+        AcompanhamentoFuncionarioModel.create(req.body)
+            .then(acompanhamento_funcionario => res.json(acompanhamento_funcionario))
+            .catch(error => res.json(error))
+    }
+    delete(req, res) {
+        AcompanhamentoFuncionarioModel.destroy({ where: { FUNCIONARIO_CODIGO: req.params.id } })
             .then(acompanhamento_funcionario => res.json(acompanhamento_funcionario))
             .catch(error => res.json(error))
     }
