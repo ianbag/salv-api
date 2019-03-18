419edfe08a61519539fac49ca85251688460bcb9
hppod <hppodgornik@gmail.com>
2019-03-18 19:55:47 -0300

    gitignore

---
+++ auth/sequelize.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('salv-bd', 'admin-dev', 'salv2018gpes10', {
+    host: "mysql995.umbler.com",
+    port: "41890",
+    dialect: "mysql"
+})
+
+module.exports = sequelize
