b8166ed34d64d145b4fa990df17f6a9cc99c8ecc
hppod <hppodgornik@gmail.com>
2019-03-30 14:46:41 -0300

    remote database

--- app/controllers/convenio.js
+++ app/controllers/convenio.js
@@ -1,4 +1,4 @@
-const sequelize = require('../../database/sequelize_local')
+const sequelize = require('../../database/sequelize_remote')
 const { ConvenioModel } = require('./../models')
 
 class Convenio {
--- app/controllers/residente_convenio.js
+++ app/controllers/residente_convenio.js
@@ -1,4 +1,4 @@
-const sequelize = require('../../database/sequelize_local')
+const sequelize = require('../../database/sequelize_remote')
 
 const { ResidenteConvenioModel, ResidenteModel, ConvenioModel } = require('../models')
 
