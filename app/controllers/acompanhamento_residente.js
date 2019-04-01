4d1299e58b1b342b261ff69dac01dd398f01e732
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-04-01 11:12:55 -0300

    Alterado metodo create de acompanhamento residente para receber um array e inserir varios, alterado metodo get: retornando varios em um array

--- app/controllers/acompanhamento_residente.js
+++ app/controllers/acompanhamento_residente.js
@@ -1,31 +1,44 @@
-const { AcompanhamentoResidenteModel, AcompanhamentosModel, ResidenteModel } = require('./../models')
+const sequelize = require('../../database/sequelize_remote')
 
-AcompanhamentoResidenteModel.belongsTo(AcompanhamentosModel, {as: 'ACOMPANHAMENTO', foreignKey:'ACOMPANHAMENTO_CODIGO'})
-AcompanhamentoResidenteModel.belongsTo(ResidenteModel, {as: 'RESIDENTE', foreignKey:'RESIDENTE_CODIGO'})
+const { AcompanhamentoResidenteModel } = require('./../models')
 
-class AcompanhamentoResidente{
+class AcompanhamentoResidente {
 
-    getById (req, res) {
-        AcompanhamentoResidenteModel.findById(req.params.id, {
-            include: [
-                {model: AcompanhamentosModel, as: 'ACOMPANHAMENTO'},
-                {model: ResidenteModel, as: 'RESIDENTE'}
-            ]
-        })
-            .then(acompanhamento_residente => res.json(acompanhamento_residente))
-            .catch(error => res.json(error))
+    getById(req, res) {
+        sequelize.query(`SELECT
+                            A.CODIGO,
+                            P.NOME, P.SOBRENOME,
+                            R.APELIDO
+                        FROM
+                            ACOMPANHAMENTO A
+                            LEFT JOIN ACOMPANHAMENTO_RESIDENTE AR
+                            ON AR.ACOMPANHAMENTO_CODIGO = A.CODIGO
+                            LEFT JOIN RESIDENTE R
+                            ON R.CODIGO_RESIDENTE = AR.RESIDENTE_CODIGO
+                            LEFT JOIN PESSOA P
+                            ON P.CODIGO = R.PESSOA_CODIGO
+                            WHERE A.CODIGO = ACOMPANHAMENTO_CODIGO`,
+            { replacements: { ACOMPANHAMENTO_CODIGO: req.params.id } })
+            .then(result => {
+                res.json(result[0])
+            })
     }
     create(req, res) {
-        AcompanhamentoResidenteModel.create(req.body)
-            .then(acompanhamento_residente => res.json(acompanhamento_residente))
-            .catch(error => res.json(error))
+        let count = 0;
+        req.body.forEach(element => {
+            AcompanhamentoResidenteModel.create(element)
+                .then()
+                .catch(error => res.json(error))
+            count++
+        })
+        res.json({ message: `Foram adicionados ${count} Acompanhamentos Residentes` })
     }
     delete(req, res) {
         AcompanhamentoResidenteModel.destroy({ where: { RESIDENTE_CODIGO: req.params.id } })
             .then(acompanhamento_residente => res.json(acompanhamento_residente))
             .catch(error => res.json(error))
     }
-    
-    
+
+
 }
-module.exports = new  AcompanhamentoResidente()
+module.exports = new AcompanhamentoResidente()
