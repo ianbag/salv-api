92dca2078837fbc2f0e5901900d2e45c6922196e
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-04-03 12:59:43 -0300

    Corrigido objeto de cadastro Telefone

--- app/controllers/telefone.js
+++ app/controllers/telefone.js
@@ -49,7 +49,7 @@ class Telefone {
     createTelefoneFamiliar(req, res) {
         let count = 0;
         req.body.telefones.forEach((element, index, array) => {
-            TelefoneModel.create(element)
+            TelefoneModel.create({NUMERO: element})
                 .then(telefone => {
                     let TELEFONE_FAMILIAR = {
                         FAMILIAR_CODIGO: req.body.familiar,
