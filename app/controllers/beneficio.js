e5241acc3f82b871f10243a0d69bb5f2803e0c9a
hppod <hppodgornik@gmail.com>
2019-04-03 23:37:55 -0300

    instancia de beneficio

--- app/controllers/beneficio.js
+++ app/controllers/beneficio.js
@@ -1,9 +1,4 @@
-const Sequelize = require('sequelize')
-let sequelize = new Sequelize('salv-bd', 'admin-dev', 'salv2018gpes10', {
-    host: "mysql995.umbler.com",
-    port: "41890",
-    dialect: "mysql"
-})
+const sequelize = require('./../database/sequelize_remote')
 
 /**
  * @author Ian Rotondo Bagliotti
@@ -18,19 +13,19 @@ const { BeneficioModel } = require('./../models')
 class Beneficio {
     get(req, res) {
         BeneficioModel.findAll({
-             raw: true,
-             where: {STATUS: 0}
-            })
+            raw: true,
+            where: { STATUS: 0 }
+        })
             .then(beneficio => res.json(beneficio))
             .catch(error => res.json(error))
     }
 
     getByName(req, res) {
-        BeneficioModel.findOne({ 
+        BeneficioModel.findOne({
             where: {
-             NOME_BENEFICIO: req.params.name ,
-             STATUS: 0
-            } 
+                NOME_BENEFICIO: req.params.name,
+                STATUS: 0
+            }
         })
             .then(beneficio => res.json(beneficio))
             .catch(error => res.json(error))
@@ -43,7 +38,7 @@ class Beneficio {
     }
 
     update(req, res) {
-        BeneficioModel.update(req.body, { 
+        BeneficioModel.update(req.body, {
             where: {
                 NOME_BENEFICIO: req.params.name,
                 STATUS: 0
@@ -54,7 +49,7 @@ class Beneficio {
     }
 
     delete(req, res) {
-        BeneficioModel.update({STATUS: 1}, {
+        BeneficioModel.update({ STATUS: 1 }, {
             where: {
                 NOME_BENEFICIO: req.params.name,
                 STATUS: 0
@@ -64,7 +59,7 @@ class Beneficio {
             .catch(error => res.json(error))
     }
 
-    provaDeVida(req, res){
+    provaDeVida(req, res) {
         let data = new Date()
         sequelize.query(`SELECT
                             B.NOME_BENEFICIO, B.PROVA_VIDA_BENEFICIO,
@@ -77,7 +72,7 @@ class Beneficio {
                             ON P.CODIGO = R.PESSOA_CODIGO
                             WHERE MONTH(B.PROVA_VIDA_BENEFICIO) = :mes
                             AND YEAR(B.PROVA_VIDA_BENEFICIO) = :ano`,
-            { replacements: { mes: (data.getMonth()+1), ano: data.getFullYear() } })
+            { replacements: { mes: (data.getMonth() + 1), ano: data.getFullYear() } })
             .then(result => {
                 res.json(result[0])
             })
