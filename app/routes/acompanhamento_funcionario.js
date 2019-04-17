de675bff5aeaf0406562fee4b3fe5a8ba30fd674
Juliano <juliano-gema@hotmail.com>
2019-04-17 01:25:46 -0300

    metodos delete acompanhamento funcionario e residente

--- app/routes/acompanhamento_funcionario.js
+++ app/routes/acompanhamento_funcionario.js
@@ -4,6 +4,6 @@ const route = express.Router()
 const AcompanhamentoFuncionarioController = require('../../../salv-api/app/controllers/acompanhamento_funcionario')
 route.get('/acompanhamento_funcionario/:id', AcompanhamentoFuncionarioController.getById)
 route.post('/acompanhamento_funcionario', AcompanhamentoFuncionarioController.create)
-route.delete('/acompanhamento_funcionario/:id', AcompanhamentoFuncionarioController.delete)
+route.delete('/acompanhamento_funcionario/:idFuncionario/:idAcompanhamento', AcompanhamentoFuncionarioController.delete)
 
 module.exports = route
