a7f94045d758ca61e57f425e0b9046e5578b1330
hppod <hppodgornik@gmail.com>
2019-04-21 18:47:29 -0300

    relatorio de acompanhamentos

--- reports/controller/relatorios.js
+++ reports/controller/relatorios.js
@@ -1,8 +1,9 @@
 const sequelize = require('./../../database/sequelize_remote')
 const request = require('request')
+const REPORT_API = 'http://localhost:3660/api/report'
 class Relatorios {
 
-    funcionarios(req, res){
+    funcionarios(req, res) {
         sequelize.query(
             `SELECT P.NOME, P.SOBRENOME, MASK(P.RG, "##.###.###-#") AS RG, MASK(P.CPF, "###.###.###-##") AS CPF, DATE_FORMAT(P.DATA_NASCIMENTO, "%d/%m/%Y") AS DATA_NASCIMENTO, F.CARGO, DATE_FORMAT(F.DATA_ADMISSAO, "%d/%m/%Y") AS DATA_ADMISSAO 
             FROM PESSOA AS P
@@ -11,19 +12,19 @@ class Relatorios {
             WHERE F.STATUS = 1
             ORDER BY P.DATA_NASCIMENTO`
         ).then(result => {
-            
+
             var data = {
-                "template": {"name": "relatorio-de-funcionarios"},
+                "template": { "name": "relatorio-de-funcionarios" },
                 "data": {
                     "funcionarios": result[0]
                 },
-                options:{
+                options: {
                     preview: true
                 }
             }
 
             var options = {
-                uri: 'http://localhost:3660/api/report',
+                uri: REPORT_API,
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -32,7 +33,45 @@ class Relatorios {
             }
             //res.send(data)
             request(options).pipe(res)
-        })
+        }).catch(error => res.json(error))
+    }
+
+    acompanhamentos(req, res) {
+        sequelize.query(
+            `/**ACOMPANHAMENTOS**/
+            SELECT DATE_FORMAT(A.DATA_ACOMPANHAMENTO, "%d/%m/%Y") AS DATA_ACOMPANHAMENTO, 
+            A.ATIVIDADE, 
+                (SELECT COUNT(CODIGO_RESIDENTE) 
+                FROM ACOMPANHAMENTO_RESIDENTE 
+                WHERE ACOMPANHAMENTO_CODIGO = A.CODIGO) AS NUMERO_RESIDENTES_PARTICIPANTES ,
+                    (SELECT COUNT(CODIGO_FUNCIONARIO)
+                    FROM ACOMPANHAMENTO_FUNCIONARIO
+                    WHERE ACOMPANHAMENTO_CODIGO = A.CODIGO) AS NUMERO_FUNCIONARIOS_PARTICIPANTES
+            FROM ACOMPANHAMENTO AS A
+            ORDER BY A.DATA_ACOMPANHAMENTO;`
+        ).then(result => {
+
+            var data = {
+                "template": { "name": "relatorio-de-acompanhamentos" },
+                "data": {
+                    "acompanhamentos": result[0]
+                },
+                options: {
+                    preview: true
+                }
+            }
+
+            var options = {
+                uri: REPORT_API,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                json: data
+            }
+
+            request(options).pipe(res)
+        }).catch(error => res.json(error))
     }
 
 }
--- reports/routes/relatorios.routes.js
+++ reports/routes/relatorios.routes.js
@@ -3,6 +3,7 @@ const route = express.Router()
 
 const RelatorioController = require('./../controller/relatorios')
 
-route.get('/relatorio_funcionarios', RelatorioController.funcionarios)
+route.get('/relatorio_funcionarios', RelatorioController.funcionarios),
+route.get('/relatorio_acompanhamentos', RelatorioController.acompanhamentos)
 
 module.exports = route
