9fd1d7f775c691e63255b0175f6e3bcb48a08bc9
hppod <hppodgornik@gmail.com>
2019-04-21 21:21:42 -0300

    Relatorio de convenios

--- reports/controller/relatorios.js
+++ reports/controller/relatorios.js
@@ -5,12 +5,21 @@ class Relatorios {
 
     funcionarios(req, res) {
         sequelize.query(
-            `SELECT P.NOME, P.SOBRENOME, MASK(P.RG, "##.###.###-#") AS RG, MASK(P.CPF, "###.###.###-##") AS CPF, DATE_FORMAT(P.DATA_NASCIMENTO, "%d/%m/%Y") AS DATA_NASCIMENTO, F.CARGO, DATE_FORMAT(F.DATA_ADMISSAO, "%d/%m/%Y") AS DATA_ADMISSAO 
-            FROM PESSOA AS P
-            INNER JOIN FUNCIONARIO AS F
-            ON F.PESSOA_CODIGO = P.CODIGO
-            WHERE F.STATUS = 1
-            ORDER BY P.DATA_NASCIMENTO`
+            `SELECT 
+            P.NOME,
+            P.SOBRENOME,
+            MASK(P.RG, '##.###.###-#') AS RG,
+            MASK(P.CPF, '###.###.###-##') AS CPF,
+            DATE_FORMAT(P.DATA_NASCIMENTO, '%d/%m/%Y') AS DATA_NASCIMENTO,
+            F.CARGO,
+            DATE_FORMAT(F.DATA_ADMISSAO, '%d/%m/%Y') AS DATA_ADMISSAO
+        FROM
+            PESSOA AS P
+                INNER JOIN
+            FUNCIONARIO AS F ON F.PESSOA_CODIGO = P.CODIGO
+        WHERE
+            F.STATUS = 1
+        ORDER BY P.NOME;`
         ).then(result => {
 
             var data = {
@@ -38,23 +47,73 @@ class Relatorios {
 
     acompanhamentos(req, res) {
         sequelize.query(
-            `/**ACOMPANHAMENTOS**/
-            SELECT DATE_FORMAT(A.DATA_ACOMPANHAMENTO, "%d/%m/%Y") AS DATA_ACOMPANHAMENTO, 
-            A.ATIVIDADE, 
-                (SELECT COUNT(CODIGO_RESIDENTE) 
-                FROM ACOMPANHAMENTO_RESIDENTE 
-                WHERE ACOMPANHAMENTO_CODIGO = A.CODIGO) AS NUMERO_RESIDENTES_PARTICIPANTES ,
-                    (SELECT COUNT(CODIGO_FUNCIONARIO)
-                    FROM ACOMPANHAMENTO_FUNCIONARIO
-                    WHERE ACOMPANHAMENTO_CODIGO = A.CODIGO) AS NUMERO_FUNCIONARIOS_PARTICIPANTES
-            FROM ACOMPANHAMENTO AS A
-            ORDER BY A.DATA_ACOMPANHAMENTO;`
+            `SELECT 
+            DATE_FORMAT(A.DATA_ACOMPANHAMENTO, '%d/%m/%Y') AS DATA_ACOMPANHAMENTO,
+            A.ATIVIDADE,
+            (SELECT 
+                    COUNT(CODIGO_RESIDENTE)
+                FROM
+                    ACOMPANHAMENTO_RESIDENTE
+                WHERE
+                    ACOMPANHAMENTO_CODIGO = A.CODIGO) AS NUMERO_RESIDENTES_PARTICIPANTES,
+            (SELECT 
+                    COUNT(CODIGO_FUNCIONARIO)
+                FROM
+                    ACOMPANHAMENTO_FUNCIONARIO
+                WHERE
+                    ACOMPANHAMENTO_CODIGO = A.CODIGO) AS NUMERO_FUNCIONARIOS_PARTICIPANTES
+        FROM
+            ACOMPANHAMENTO AS A
+        ORDER BY A.DATA_ACOMPANHAMENTO;`
         ).then(result => {
 
             var data = {
                 "template": { "name": "relatorio-de-acompanhamentos" },
                 "data": {
                     "acompanhamentos": result[0]
+                },
+                options: {
+                    preview: true
+                }
+            }
+
+            var options = {
+                uri: REPORT_API,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                json: data
+            }
+
+            request(options).pipe(res)
+        }).catch(error => res.json(error))
+    }
+
+    convenios(req, res) {
+        sequelize.query(
+            `SELECT 
+            C.NOME_CONVENIO,
+            C.TIPO_CONVENIO,
+            T.DDD AS DDD,
+            IF(CHARACTER_LENGTH(T.NUMERO = '9'),
+                MASK(T.NUMERO, '#####-####'),
+                MASK(T.NUMERO, '####-####')) AS NUMERO
+        FROM
+            CONVENIO AS C
+                INNER JOIN
+            TELEFONE_CONVENIO AS TC ON TC.CONVENIO_CODIGO = C.CODIGO
+                INNER JOIN
+            TELEFONE AS T ON T.CODIGO = TC.TELEFONE_CODIGO
+        WHERE
+            C.STATUS = 1
+        ORDER BY C.NOME_CONVENIO;`
+        ).then(result => {
+
+            var data = {
+                "template": { "name": "relatorio-de-convenios" },
+                "data": {
+                    "convenios": result[0]
                 },
                 options: {
                     preview: true
--- reports/routes/relatorios.routes.js
+++ reports/routes/relatorios.routes.js
@@ -4,6 +4,7 @@ const route = express.Router()
 const RelatorioController = require('./../controller/relatorios')
 
 route.get('/relatorio_funcionarios', RelatorioController.funcionarios),
-route.get('/relatorio_acompanhamentos', RelatorioController.acompanhamentos)
+route.get('/relatorio_acompanhamentos', RelatorioController.acompanhamentos),
+route.get('/relatorio_convenios', RelatorioController.convenios)
 
 module.exports = route
