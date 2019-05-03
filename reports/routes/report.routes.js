7c0cf9ed554eabd4a7decce6872e0e989928f05f
hppod <hppodgornik@gmail.com>
2019-05-02 23:34:57 -0300

    Definindo a rota que acessará a função de relatório dos convenios

--- reports/routes/report.routes.js
+++ reports/routes/report.routes.js
@@ -4,10 +4,19 @@ const route = express.Router()
 
 //Require function
 const reportFuncionario = require('../controllers/reports')
+const reportConvenio = require('../controllers/reports')
 
 //HTTP method, call function
 route.get('/relatorio-funcionarios', function (req, res) {
     reportFuncionario().then(response => {
+        res.type('application/pdf')
+        res.send(response)
+    })
+})
+
+//HTTP method, call function
+route.get('/relatorio-convenios', function (req, res) {
+    reportConvenio().then(response => {
         res.type('application/pdf')
         res.send(response)
     })
