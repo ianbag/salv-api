9366a71ebd307b552edb69a1431bc0e35041524f
hppod <hppodgornik@gmail.com>
2019-05-03 00:01:40 -0300

    Definindo a rota que acessará a função responsável pelos relatorios de residentes /  Adicionando catch nas condições then

--- reports/routes/report.routes.js
+++ reports/routes/report.routes.js
@@ -3,13 +3,15 @@ const express = require('express')
 const route = express.Router()
 
 //Require function
-const { reportFuncionario, reportConvenio, reportAcompanhamento } = require('../controllers/reports')
+const { reportFuncionario, reportConvenio, reportAcompanhamento, reportResidente } = require('../controllers/reports')
 
 //HTTP method, call function
 route.get('/relatorio-funcionarios', function (req, res) {
     reportFuncionario().then(response => {
         res.type('application/pdf')
         res.send(response)
+    }).catch(error => {
+        res.send(error)
     })
 })
 
@@ -18,6 +20,8 @@ route.get('/relatorio-convenios', function (req, res) {
     reportConvenio().then(response => {
         res.type('application/pdf')
         res.send(response)
+    }).catch(error => {
+        res.send(error)
     })
 })
 
@@ -26,6 +30,18 @@ route.get('/relatorio-acompanhamentos', function (req, res) {
     reportAcompanhamento().then(response => {
         res.type('application/pdf')
         res.send(response)
+    }).catch(error => {
+        res.send(error)
+    })
+})
+
+//HTTP method, call function
+route.get('/relatorio-residentes', function (req, res) {
+    reportResidente().then(response => {
+        res.type('application/pdf')
+        res.send(response)
+    }).catch(error => {
+        res.send(error)
     })
 })
 
