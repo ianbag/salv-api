51a06b02f3bc32ddd2c97fc70237725c18df0dcc
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-05-05 17:07:40 -0300

    corrigido problema de merge de beneficios

--- app/controllers/beneficio.js
+++ app/controllers/beneficio.js
@@ -1,5 +1,3 @@
-const sequelize = require('./../../database/sequelize_remote')
-
 /**
  * @author Ian Rotondo Bagliotti
  * @email ian.bagliotti@gmail.com
@@ -8,13 +6,20 @@ const sequelize = require('./../../database/sequelize_remote')
  * @desc Controller da API de Beneficio
  */
 
+const sequelize = require('./../../database/sequelize_remote')
+
+const OP = sequelize.Op 
+
 const { BeneficioModel } = require('./../models')
 
 class Beneficio {
     get(req, res) {
         BeneficioModel.findAll({
             raw: true,
-            where: { STATUS: 1 }
+            where: {
+                CODIGO_RESIDENTE: req.params.id,
+                STATUS: 1 
+                }
         })
             .then(beneficio => res.json(beneficio))
             .catch(error => res.json(error))
@@ -23,6 +28,7 @@ class Beneficio {
     getByName(req, res) {
         BeneficioModel.findOne({
             where: {
+                CODIGO_RESIDENTE: req.params.id,
                 NOME_BENEFICIO: req.params.name,
                 STATUS: 1
             }
@@ -40,6 +46,7 @@ class Beneficio {
     update(req, res) {
         BeneficioModel.update(req.body, {
             where: {
+                CODIGO_RESIDENTE: req.params.id,
                 NOME_BENEFICIO: req.params.name,
                 STATUS: 1
             }
@@ -51,6 +58,7 @@ class Beneficio {
     delete(req, res) {
         BeneficioModel.update({ STATUS: 0 }, {
             where: {
+                CODIGO_RESIDENTE: req.params.id,
                 NOME_BENEFICIO: req.params.name,
                 STATUS: 1
             }
@@ -71,12 +79,32 @@ class Beneficio {
                             LEFT JOIN PESSOA P
                             ON P.CODIGO = R.PESSOA_CODIGO
                             WHERE MONTH(B.PROVA_VIDA_BENEFICIO) = :mes
-                            AND YEAR(B.PROVA_VIDA_BENEFICIO) = :ano
+                            AND YEAR(B.PROVA_VIDA_BENEFICIO) = :ano 
+                            AND B.STATUS = 1
                             ORDER BY DAY(B.PROVA_VIDA_BENEFICIO) ASC`,
             { replacements: { mes: (data.getMonth() + 1), ano: data.getFullYear() } })
             .then(result => {
                 res.json(result[0])
             })
+    }
+
+    uniqueNome(req, res) {
+        BeneficioModel.findOne({
+            raw: true,
+            where: {
+                STATUS: 1,
+                CODIGO_RESIDENTE: req.body.CODIGO,
+                NOME_BENEFICIO:  req.body.NOME_BENEFICIO ,
+            }
+        })
+            .then(beneficio => {
+                if (beneficio && req.body.NOME_BENEFICIO != req.body.NOME_BENEFICIO_EDITAR)
+                    res.json({beneficio, value: 0, message: 'Nome Benefício não é único!' })
+
+                else
+                    res.json({ value: 1, message: 'Nome Benefício é único!' })
+            })
+            .catch(error => res.json(error))
     }
 }
 
