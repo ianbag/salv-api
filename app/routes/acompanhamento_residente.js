e4ef4840036f3536f708f6d70d2ef2147e9fc573
hppod <hppodgornik@gmail.com>
2019-05-06 13:59:54 -0300

    Fix route acompanhamento residente controller

--- app/routes/acompanhamento_residente.js
+++ app/routes/acompanhamento_residente.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = express.Router()
 
-const AcompanhamentoResidenteController = require('../../../salv-api/app/controllers/acompanhamento_residente')
+const AcompanhamentoResidenteController = require('./../controllers/acompanhamento_residente')
 route.get('/acompanhamento_residente/:id', AcompanhamentoResidenteController.getById)
 route.post('/acompanhamento_residente', AcompanhamentoResidenteController.create)
 route.delete('/acompanhamento_residente/:idResidente/:idAcompanhamento', AcompanhamentoResidenteController.delete)
