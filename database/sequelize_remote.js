b9e10975e50f5d24e5c8826cb8744df394735fc5
hppod <hppodgornik@gmail.com>
2019-05-16 22:03:05 -0300

    Set cloud database and local database

--- database/sequelize_remote.js
+++ database/sequelize_remote.js
@@ -1,8 +1,20 @@
+//REMOTO
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize('salv-bd', 'admin-dev', 'salv2018gpes10', {
+//     host: "mysql995.umbler.com",
+//     port: "41890",
+//     dialect: "mysql"
+// })
+
+// module.exports = sequelize
+
+//LOCAL
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('salv-bd', 'admin-dev', 'salv2018gpes10', {
-    host: "mysql995.umbler.com",
-    port: "41890",
+const sequelize = new Sequelize('salv-bd', 'root', null, {
+    host: "127.0.0.1",
+    port: "3306",
     dialect: "mysql"
 })
 
