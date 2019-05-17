8dbedc4c7631f1945f7e3ae0ff05a33a0c51a716
hppod <hppodgornik@gmail.com>
2019-05-16 21:57:27 -0300

    Adding local and remote instance sql

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(cors())
 
