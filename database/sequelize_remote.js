4359fcdeb7676e3333cbc8663ebb636e2195ab6f
hppod <hppodgornik@gmail.com>
2019-05-19 10:38:59 -0300

    Remote database

--- database/sequelize_remote.js
+++ database/sequelize_remote.js
@@ -1,21 +1,21 @@
-//REMOTO
-// const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize('salv-bd', 'admin-dev', 'salv2018gpes10', {
-//     host: "mysql995.umbler.com",
-//     port: "41890",
-//     dialect: "mysql"
-// })
-
-// module.exports = sequelize
-
-//LOCAL
+REMOTO
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('salv-bd', 'root', null, {
-    host: "127.0.0.1",
-    port: "3306",
+const sequelize = new Sequelize('salv-bd', 'admin-dev', 'salv2018gpes10', {
+    host: "mysql995.umbler.com",
+    port: "41890",
     dialect: "mysql"
 })
 
 module.exports = sequelize
+
+// //LOCAL
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize('salv-bd', 'root', null, {
+//     host: "127.0.0.1",
+//     port: "3306",
+//     dialect: "mysql"
+// })
+
+// module.exports = sequelize
