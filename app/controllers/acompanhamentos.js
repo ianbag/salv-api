1c213a6aa2f1e93f56f639e9680de1c539cdc1f9
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-05-28 20:37:12 -0300

    update method dateStart

--- app/controllers/acompanhamentos.js
+++ app/controllers/acompanhamentos.js
@@ -196,7 +196,7 @@ class Acompanhamento {
             AR.CODIGO_RESIDENTE = :CODIGO_RESIDENTE
             AND DATA_ACOMPANHAMENTO BETWEEN '${dateStart}' AND NOW()
             ORDER BY A.DATA_ACOMPANHAMENTO DESC`,
-                { replacements: { CODIGO_RESIDENTE: req.params.id } }
+                { replacements: { CODIGO_RESIDENTE: req.body.CODIGO_RESIDENTE } }
             )
                 .then((result) => res.json(result[0]))
                 .catch((error) => res.json(error))
